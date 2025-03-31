df809d1a03a2e271e98074357a02a3584273efe4
Max Herbert <w0494420@campus.nscc.ca>
2025-03-31 10:50:04 -0300

    Updated task Widget

--- task_widget.py
+++ task_widget.py
@@ -6,18 +6,24 @@ class TaskWidget(QWidget):
         super().__init__(parent)
 
         self.layout = QVBoxLayout()
+        self.maxTextLength = 60
+        self.description = description
+        self.description_text = description[:self.maxTextLength] + " ..." if len(description) > self.maxTextLength else description
 
         self.title_label = QLabel(title)
-        self.desc_box = QTextEdit(description)
-        self.desc_box.setReadOnly(True)
-        self.desc_box.setMaximumHeight(50)
+        self.desc_label = QLabel(self.description_text)
+
+        #self.desc_box = QTextEdit(description)
+        #self.desc_box.setReadOnly(True)
+        #self.desc_box.setMaximumHeight(50)
         #self.desc_box.setMaximumWidth(200)
 
         self.start_date_label = QLabel(f"Start Date: {start_date if start_date else 'N/A'}")
         self.end_date_label = QLabel(f"End Date: {end_date if end_date else 'N/A'}")
 
         self.layout.addWidget(self.title_label)
-        self.layout.addWidget(self.desc_box)
+        self.layout.addWidget(self.desc_label)
+        #self.layout.addWidget(self.desc_box)
         self.layout.addWidget(self.start_date_label)
         self.layout.addWidget(self.end_date_label)
 
@@ -27,11 +33,13 @@ class TaskWidget(QWidget):
     def get_details(self):
         start_date_text = self.start_date_label.text().replace("Start Date: ", "").strip()
         end_date_text = self.end_date_label.text().replace("End Date: ", "").strip()
-        return self.title_label.text(), self.desc_box.toPlainText(), start_date_text, end_date_text
+        return self.title_label.text(), self.description, start_date_text, end_date_text
+        #return self.title_label.text(), self.desc_box.toPlainText(), start_date_text, end_date_text
 
     # Setter for details
     def set_details(self, title, description, start_date=None, end_date=None):
         self.title_label.setText(title)
-        self.desc_box.setPlainText(description)
+        self.desc_label.setText(self.description_text)
+        #self.desc_box.setPlainText(description)
         self.start_date_label.setText(f"Start Date: {start_date if start_date else 'N/A'}")
         self.end_date_label.setText(f"End Date: {end_date if end_date else 'N/A'}")
