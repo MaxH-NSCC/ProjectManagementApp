e549c6b5984d5a2fbc1bf54d91f6cdf83c0a922c
Max Herbert <w0494420@campus.nscc.ca>
2025-03-31 11:22:10 -0300

    Removed unused code

--- task_widget.py
+++ task_widget.py
@@ -13,17 +13,11 @@ class TaskWidget(QWidget):
         self.title_label = QLabel(title)
         self.desc_label = QLabel(self.description_text)
 
-        #self.desc_box = QTextEdit(description)
-        #self.desc_box.setReadOnly(True)
-        #self.desc_box.setMaximumHeight(50)
-        #self.desc_box.setMaximumWidth(200)
-
         self.start_date_label = QLabel(f"Start Date: {start_date if start_date else 'N/A'}")
         self.end_date_label = QLabel(f"End Date: {end_date if end_date else 'N/A'}")
 
         self.layout.addWidget(self.title_label)
         self.layout.addWidget(self.desc_label)
-        #self.layout.addWidget(self.desc_box)
         self.layout.addWidget(self.start_date_label)
         self.layout.addWidget(self.end_date_label)
 
@@ -34,12 +28,10 @@ class TaskWidget(QWidget):
         start_date_text = self.start_date_label.text().replace("Start Date: ", "").strip()
         end_date_text = self.end_date_label.text().replace("End Date: ", "").strip()
         return self.title_label.text(), self.description, start_date_text, end_date_text
-        #return self.title_label.text(), self.desc_box.toPlainText(), start_date_text, end_date_text
 
     # Setter for details
     def set_details(self, title, description, start_date=None, end_date=None):
         self.title_label.setText(title)
         self.desc_label.setText(self.description_text)
-        #self.desc_box.setPlainText(description)
         self.start_date_label.setText(f"Start Date: {start_date if start_date else 'N/A'}")
         self.end_date_label.setText(f"End Date: {end_date if end_date else 'N/A'}")
