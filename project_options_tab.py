ff90c6e0afa2727d5a99528b719d91c543719222
Max Herbert <w0494420@campus.nscc.ca>
2025-04-15 10:07:52 -0300

    Updated options tab GUI

--- project_options_tab.py
+++ project_options_tab.py
@@ -1,47 +1,65 @@
-from PyQt5.QtWidgets import QWidget, QFormLayout, QLineEdit, QTextEdit, QCheckBox, QPushButton, QLabel, QMessageBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QTextEdit, QCheckBox, QPushButton, QLabel, QMessageBox, QSizePolicy
 from PyQt5.QtGui import QIntValidator
 
 class ProjectOptionsTab(QWidget):
     def __init__(self, project, parent=None):
         super().__init__(parent)
         self.project = project
-        layout = QFormLayout()
+        layout = QVBoxLayout()
 
+        # Project Title
         self.project_title = QLineEdit(self.project.title)
+        layout.addWidget(QLabel("Project Title:"))
+        layout.addWidget(self.project_title)
+
+        # Project Description
         self.project_description = QTextEdit(self.project.description)
         self.project_description.setMaximumHeight(200)
+        layout.addWidget(QLabel("Project Description:"))
+        layout.addWidget(self.project_description)
 
+        # Task and Resource Deletion Warnings
         self.task_delete_warning = QCheckBox("Show Warning when deleting tasks")
         self.res_delete_warning = QCheckBox("Show Warning when deleting resources")
         self.task_delete_warning.setChecked(self.project.settings.remove_task_warn)
         self.res_delete_warning.setChecked(self.project.settings.remove_res_warn)
+        layout.addWidget(self.task_delete_warning)
+        layout.addWidget(self.res_delete_warning)
 
+        # Default App Height
+        self.height_edit = QLineEdit(str(self.project.settings.default_height))
+        self.height_edit.setMaximumWidth(200)
+        self.height_edit.setValidator(QIntValidator(1, 9999, self))
+        layout.addWidget(QLabel("Default App Height:"))
+        layout.addWidget(self.height_edit)
+
+        # Default App Width
+        self.width_edit = QLineEdit(str(self.project.settings.default_width))
+        self.width_edit.setMaximumWidth(200)
+        self.width_edit.setValidator(QIntValidator(1, 9999, self))
+        layout.addWidget(QLabel("Default App Width:"))
+        layout.addWidget(self.width_edit)
+
+        # Start Window Maximized Checkbox
+        self.start_max = QCheckBox("Start window maximized")
+        self.start_max.setChecked(self.project.settings.start_maximized)
+        layout.addWidget(self.start_max)
+
+        # Update and Reset Buttons
         self.reset_button = QPushButton("Reset Settings")
         self.reset_button.clicked.connect(self.reset_settings)
-
         self.update_button = QPushButton("Update Settings")
         self.update_button.clicked.connect(self.update_settings)
 
-        self.height_edit = QLineEdit(str(self.project.settings.default_height))
-        self.height_edit.setValidator(QIntValidator(1, 9999, self))
-
-        self.width_edit = QLineEdit(str(self.project.settings.default_width))
-        self.width_edit.setValidator(QIntValidator(1, 9999, self))
-
-        self.start_max = QCheckBox("Start window maximized")
-        self.start_max.setChecked(self.project.settings.start_maximized)
-
-        layout.addRow("Project Title:", self.project_title)
-        layout.addRow("Project Description:", self.project_description)
-        layout.addWidget(self.task_delete_warning)
-        layout.addWidget(self.res_delete_warning)
-        layout.addWidget(QLabel("Default App Height"))
-        layout.addWidget(self.height_edit)
-        layout.addWidget(QLabel("Default App Width"))
-        layout.addWidget(self.width_edit)
-        layout.addWidget(self.start_max)
+        # Group the buttons together
         layout.addWidget(self.update_button)
         layout.addWidget(self.reset_button)
+
+        # Set the layout's maximum height
+        self.setMaximumHeight(600)
+
+        # Set the layout size policy to prevent expanding vertically
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         self.setLayout(layout)
 
